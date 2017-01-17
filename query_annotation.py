4992b06dd83f8aacea7aa0c94eaa4652b1653a28
Danilo Nunes <nunesdanilo@gmail.com>
2017-01-17 09:55:22 -0200

    include image list in query annotations

--- query_annotation.py
+++ query_annotation.py
@@ -1,8 +1,12 @@
 # coding: utf-8
+import os
+from glob import glob
+
 
 class QueryAnnotation:
     def __init__(self, path):
         self.db = {}
+        self.imgs = {}
         for folder in glob(os.path.join(path, '*')):
             for fname in glob(os.path.join(folder, '*.txt')):
                 key = os.path.basename(folder) 
@@ -12,6 +16,10 @@ class QueryAnnotation:
         names1, preposition, names2 = zip(*(query.split('-') for query in self.db))
         self.names = list(set(names1) | set(names2))
         self.preposition = list(set(preposition))
+
+        for k, imlist in self.db.iteritems():
+            for imname in imlist:
+                self.imgs.setdefault(imname, []).append(k)
         
-    def __getitem__(self, term):
-        return self.db.get(term, [])
+    def __getitem__(self, imname):
+        return self.imgs.get(imname, [])
