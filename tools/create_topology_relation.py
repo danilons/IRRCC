32dbb29780c69007a98fee4ad87f736db09655b1
Danilo Nunes <nunesdanilo@gmail.com>
2017-01-19 09:07:51 -0200

    create requirements

--- tools/create_topology_relation.py
+++ tools/create_topology_relation.py
@@ -4,26 +4,35 @@ import glob
 import click
 import argparse
 import json
-from dataset import Dataset
+from irrcc.dataset import Dataset
 
 
 def store_topology(dset, output_file):
-    relations = {}
+    relations1 = {}
+    relations2 = {}
     with click.progressbar(length=len(dset.images), show_pos=True, show_percent=True) as bar:
         for image in dset.images:
             try: 
-                topology = dset.topology_relation(image)
+                topology1 = dset.objects.topology_relation(image)
+                topology2 = dset.segmentation.topology_relation(image)
             except KeyError:
                 bar.update(1)
                 continue
             relations[image] = []
-            for box1, box2, relation in topology:
-                relations[image].append({'box1': box1.tolist(), 
-                                         'box2': box2.tolist(), 'rcc': relation})
+            for box1, box2, relation in topology1:
+                relations1[image].append({'box1': box1.tolist(), 
+                                          'box2': box2.tolist(), 'rcc': relation})
+
+            for cnt1, cnt2, relation in topology2:
+                relations1[image].append({'box1': cnt1.tolist(), 
+                                          'box2': cnt2.tolist(), 'rcc': relation})
             bar.update(1)
 
-    with open(output_file, 'w') as fp:
-        json.dump(relations, fp)
+    with open(output_file.replace('#', 'boxes'), 'w') as fp:
+        json.dump(relations1, fp)
+
+    with open(output_file.replace('#', 'segment'), 'w') as fp:
+        json.dump(relations2, fp)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='IRRCC program')
@@ -41,7 +50,7 @@ if __name__ == "__main__":
         print("Processing mode: {}".format(mode))
 
         dset = Dataset(params.dataset_path, mode, params.image_path)
-        output_file = fname.replace('dataset_', 'topology_').replace('hdf5', 'json')
+        output_file = fname.replace('dataset_', 'topology_#').replace('hdf5', 'json')
         store_topology(dset, output_file)
 
     print("Done.")
