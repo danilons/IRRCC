a633df2083449b530ba843165ece27ef15a8716f
Danilo Nunes <nunesdanilo@gmail.com>
2017-04-13 21:14:57 -0300

    do not save segmented images to hdf5 to improve performance

--- tools/segment_objects.py
+++ tools/segment_objects.py
@@ -8,11 +8,16 @@ import caffe
 import h5py
 
 
-def segment_objects(net, input_file, image_path, output_file):
+def segment_objects(net, input_file, image_path, output_folder):
     hdf5 = h5py.File(input_file)
-    objects_hdf5 = h5py.File(output_file, 'w')
     with click.progressbar(length=len(hdf5), show_pos=True, show_percent=True) as bar:
         for imname in hdf5:
+            imout = os.path.join(output_folder, imname.replace('.jpg', '.png'))
+            if os.path.exists(imout):
+                print("Skipping: {}".format(imname))
+                bar.update(1)
+                continue
+
             im = cv2.imread(os.path.join(image_path, imname))
             if im is None:
                 print("Unable to read image: {}".format(os.path.join(image_path, imname)))
@@ -27,20 +32,16 @@ def segment_objects(net, input_file, image_path, output_file):
             transformer.set_raw_scale('data', 255)
             transformer.set_channel_swap('data', (2, 1, 0))
 
-            # net.blobs['data'].reshape(1, 3, 384, 384)
-
             transformed_image = transformer.preprocess('data', im)
-
             net.blobs['data'].data[...] = transformed_image
 
             output = net.forward()
             segmentation = output['fc_final_up'][0].argmax(axis=0)
-            objects_hdf5.create_dataset(imname, data=segmentation)
-
+            imout = os.path.join(output_folder, imname.replace('.jpg', '.png'))
+            cv2.imwrite(imout, segmentation)
             bar.update(1)
 
-    objects_hdf5.close()
-    print("Saved objects to: {}".format(output_file))
+    print("Done.")
 
 
 if __name__ == "__main__":
@@ -49,6 +50,7 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--prototxt', action="store", default='data/sceneparsing/deploy_DilatedNet.prototxt')
     parser.add_argument('-w', '--weights', action="store", default='data/sceneparsing/DilatedNet.caffemodel')
     parser.add_argument('-i', '--image_path', action="store", default='images')
+    parser.add_argument('-o', '--output_folder', action="store", default='segmentation')
     parser.add_argument('--gpu', dest='gpu', action="store_true", default=True)
     parser.add_argument('--no-gpu', dest='gpu', action='store_false')
     parser.set_defaults(feature=True)
@@ -62,7 +64,6 @@ if __name__ == "__main__":
     files = glob.glob(os.path.join(params.dataset_path, 'dataset_*.hdf5'))
     for fname in files:
         print("Start processing file: {}".format(fname))
-        output_file = fname.replace('dataset_', 'segmentation_')
-        segment_objects(net, fname, params.image_path, output_file)
+        segment_objects(net, fname, params.image_path, params.output_folder)
 
     print("Done.")
